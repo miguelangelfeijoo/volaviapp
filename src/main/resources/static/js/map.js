23b93782a22ed3344ebb3e20ca9ffb40f7b57de6
miguelangelfeijoo <miguelangel.feijoo@gmail.com>
2025-05-31 01:27:53 +0200

    cambio BASE_URL en map.js

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,5 +1,6 @@
 // Constantes globales
-const BASE_URL = 'http://localhost:9000/';
+//const BASE_URL = 'http://localhost:9000/';
+const BASE_URL = 'https://volaviapp.onrender.com/';
 const MAX_INITIALIZATION_ATTEMPTS = 3;
 const MAP_STYLES = {
 	default: {
